fddf247ffe731d55dc2172a942497946ef8b0cce
tchsiehfranktung <tungchengh@gmail.com>
2019-04-15 11:24:30 +0800

    second

--- class02/ViewController.swift
+++ class02/ViewController.swift
@@ -14,10 +14,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
    
-        //test
+        var str = "長榮大學"
+        let numberA = 2
+        var numberB = 3
         
+        var total : Int
+        total = numberA + numberB
+       // print(total)
     
+        /*
+        if ( numberA > 2)
+        {
+            print("這個數值的大於等於ㄉ2")
+        }
+        else
+        {
+            print("這個數值的小於等於ㄉ2")
+        }
+        */
+        
+
+        
+        func test(test1:Int, test2:Int)
+        {
+            print("test")
+        }
     }
+    
 
 
 }
