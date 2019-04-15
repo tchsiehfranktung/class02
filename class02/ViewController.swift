c2cdddb3f6b0542b1bb08ebaeaec9bda93af64bb
tchsiehfranktung <tungchengh@gmail.com>
2019-04-15 10:34:40 +0800

    first

---
+++ class02/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  class02
+//
+//  Created by HUI FENTU on 2019/4/15.
+//  Copyright © 2019 HUI FENTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+        //test
+        
+    
+    }
+
+
+}
+
